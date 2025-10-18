43c3743f22c2b6f058f0e011e094b33deac7ebd0
harry586 <38090655+harry586@users.noreply.github.com>
2025-10-18 16:12:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 更换为 coolsnowwolf/lede 源码
+sed -i 's/P3TERX\/Actions-OpenWrt/coolsnowwolf\/lede/g' .config
+sed -i 's/^# CONFIG_TARGET_ar71xx is not set/CONFIG_TARGET_ar71xx=y/g' .config
+sed -i 's/^CONFIG_TARGET_x86=y/# CONFIG_TARGET_x86 is not set/g' .config
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加自定义软件源
+echo "src/gz custom_packages file://packages" >> feeds.conf.default
+
+# 更新并安装 feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 添加自定义包到编译系统
+if [ -d "../files/packages" ]; then
+    mkdir -p package/custom
+    cp -r ../files/packages/* package/custom/ 2>/dev/null || true
+fi
